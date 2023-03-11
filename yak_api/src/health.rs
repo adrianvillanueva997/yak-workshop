bebe3902bbe0011a2e78039da071c2de00a3b9ad
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-03-11 12:16:15 +0100

    added dependencies and some entrypoints

--- yak_api/src/health.rs
+++ yak_api/src/health.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, HttpResponse, Responder};
 
-#[get("/")]
+#[get("/health")]
 async fn health() -> impl Responder {
     HttpResponse::Ok()
 }
--- yak_api/src/main.rs
+++ yak_api/src/main.rs
@@ -1,19 +1,11 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 mod health;
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+mod metrics;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(health::health)
-        // .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(health::health).service(metrics::metrics))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
-//
