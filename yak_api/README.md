970bff5d6188fe06824780e8cb24c794641b60b0
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-03-11 13:26:06 +0100

    added postgres connection

---
+++ yak_api/README.md
@@ -0,0 +1,9 @@
+# Yak api
+
+## Postgres connection
+
+To connect to postgres make sure you have an environment variable named $POSTGRES_URL defined like this:
+
+```bash
+export POSTGRES_URL=postgres://postgres:postgres@localhost:5432/postgres
+```
--- yak_api/src/main.rs
+++ yak_api/src/main.rs
@@ -1,11 +1,41 @@
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use sqlx::{postgres::PgPoolOptions, Error, Pool, Postgres};
+
 mod health;
 mod metrics;
 
+async fn create_pg_pool() -> Result<Pool<Postgres>, Error> {
+    let database_url = std::env::var("POSTGRES_URL").expect("POSTGRES_URL must be set");
+    let pg_pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            Ok(pool)
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            Err(err)
+        }
+    };
+    pg_pool
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health::health).service(metrics::metrics))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let pg_pool = create_pg_pool().await.unwrap();
+    HttpServer::new(move || {
+        App::new()
+            .service(health::health)
+            .service(metrics::metrics)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %t %s %{User-Agent}i"))
+            .app_data(web::Data::new(pg_pool.clone()))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
