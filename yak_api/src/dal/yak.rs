578b83a0921a97b0f5df6aa2759f8fb4a14bb0f1
adrianvillanueva997 <adrianvillanueva997@gmail.com>
2023-04-06 09:12:17 +0200

    fixed Dockerfile and added redis cache

--- yak_api/src/dal/yak.rs
+++ yak_api/src/dal/yak.rs
@@ -1,15 +1,24 @@
 use actix_web::web;
 
+use redis::FromRedisValue;
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
 use tracing::instrument;
 
-#[derive(Deserialize, Serialize, FromRow)]
+#[derive(Deserialize, Serialize, FromRow, Clone, Debug)]
 pub struct Yak {
     id: i32,
     name: String,
     age: f32,
     age_last_shaved: f32,
+}
+
+impl FromRedisValue for Yak {
+    fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
+        let value: String = FromRedisValue::from_redis_value(v)?;
+        let yak: Yak = serde_json::from_str(&value).unwrap();
+        Ok(yak)
+    }
 }
 
 #[instrument]
@@ -29,7 +38,7 @@ pub async fn pgsql_fetch_all_yaks(pgsql: web::Data<PgPool>) -> Result<Vec<Yak>, 
 #[instrument]
 pub async fn pgsql_fetch_yak(pgsql: web::Data<PgPool>, id: i32) -> Result<Yak, sqlx::Error> {
     let query: QueryBuilder<Postgres> =
-        QueryBuilder::new("SELECT id,name,age from yak WHERE id = $1");
+        QueryBuilder::new("SELECT id,name,age, age_last_shaved from yak WHERE id = $1");
     match sqlx::query_as::<Postgres, Yak>(query.sql())
         .bind(id)
         .fetch_one(pgsql.as_ref())
@@ -104,11 +113,110 @@ pub async fn pgsql_update_yak(
         }
     }
 }
-
+#[instrument]
 pub async fn redis_fetch_all_yaks(
     redis: web::Data<redis::Client>,
 ) -> Result<Vec<Yak>, redis::RedisError> {
     let redis_connection = redis.get_async_connection().await;
-    todo!("Implement redis_fetch_all_yaks");
-    unimplemented!()
+    match redis_connection {
+        Ok(mut connection) => {
+            let yaks: Vec<Yak> = redis::cmd("LRANGE")
+                .arg("yaks")
+                .arg(0)
+                .arg(-1)
+                .query_async(&mut connection)
+                .await?;
+            Ok(yaks)
+        }
+        Err(err) => {
+            tracing::error!("Error: {}", err);
+            Err(err)
+        }
+    }
+}
+#[instrument]
+pub async fn redis_insert_all_yaks(
+    redis: web::Data<redis::Client>,
+    yaks: Vec<Yak>,
+) -> Result<(), redis::RedisError> {
+    let redis_connection = redis.get_async_connection().await;
+    match redis_connection {
+        Ok(mut connection) => {
+            for yak in yaks {
+                let yak_json = serde_json::to_string(&yak).unwrap();
+                redis::cmd("RPUSH")
+                    .arg("yaks")
+                    .arg(yak_json)
+                    .query_async(&mut connection)
+                    .await?;
+            }
+            redis::cmd("EXPIRE")
+                .arg("yaks")
+                .arg(60)
+                .query_async(&mut connection)
+                .await?;
+            Ok(())
+        }
+        Err(err) => {
+            tracing::error!("Error: {}", err);
+            Err(err)
+        }
+    }
+}
+#[instrument]
+pub async fn redis_fetch_yak(
+    redis: web::Data<redis::Client>,
+    id: i32,
+) -> Result<Yak, redis::RedisError> {
+    let redis_connection = redis.get_async_connection().await;
+    match redis_connection {
+        Ok(mut connection) => {
+            let yaks: Vec<Yak> = redis::cmd("LRANGE")
+                .arg("yaks")
+                .arg(0)
+                .arg(-1)
+                .query_async(&mut connection)
+                .await?;
+            let yak = yaks.iter().find(|y| y.id == id);
+            match yak {
+                Some(yak) => Ok(yak.clone()),
+                None => Err(redis::RedisError::from((
+                    redis::ErrorKind::TypeError,
+                    "Yak not found",
+                ))),
+            }
+        }
+        Err(err) => {
+            tracing::error!("Error: {}", err);
+            Err(err)
+        }
+    }
+}
+
+#[instrument]
+pub async fn redis_insert_yak(
+    redis: web::Data<redis::Client>,
+    yak: Yak,
+) -> Result<Yak, redis::RedisError> {
+    let redis_connection = redis.get_async_connection().await;
+    match redis_connection {
+        Ok(mut connection) => {
+            let yak_json = serde_json::to_string(&yak).unwrap();
+            redis::cmd("RPUSH")
+                .arg("yak".to_string() + &yak.id.to_string())
+                .arg(yak_json)
+                .query_async(&mut connection)
+                .await?;
+            redis::cmd("EXPIRE")
+                .arg("yak".to_string() + &yak.id.to_string())
+                .arg(60)
+                .query_async(&mut connection)
+                .await?;
+            Ok(yak)
+        }
+        Err(err) => {
+            tracing::error!("Error: {}", err);
+            Err(err)
+        }
+    }
 }
--- yak_api/src/routes/yak.rs
+++ yak_api/src/routes/yak.rs
@@ -1,23 +1,20 @@
 use actix_web::{web, HttpResponse, Responder};
 
 use serde::{Deserialize, Serialize};
-use sqlx::{FromRow, PgPool};
+use sqlx::PgPool;
 use tracing::instrument;
 
-use crate::dal::yak::{pgsql_create_yak, pgsql_delete_yak, pgsql_fetch_all_yaks, pgsql_fetch_yak};
+use crate::dal::yak::{
+    pgsql_create_yak, pgsql_delete_yak, pgsql_fetch_all_yaks, pgsql_fetch_yak,
+    redis_fetch_all_yaks, redis_fetch_yak, redis_insert_all_yaks, redis_insert_yak,
+};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct YakCreate {
     name: String,
     age: f32,
 }
-#[derive(Deserialize, Serialize, FromRow)]
-pub struct Yak {
-    id: i32,
-    name: String,
-    age: f32,
-    age_last_shaved: f32,
-}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct YakDelete {
     id: i32,
@@ -30,6 +27,7 @@ pub struct YakUpdate {
     age: f32,
 }
 
+/// .
 #[instrument]
 pub async fn create_yak(yak: web::Json<YakCreate>, pgsql: web::Data<PgPool>) -> HttpResponse {
     tracing::info!("Creating yak: {:?}", yak);
@@ -41,18 +39,37 @@ pub async fn create_yak(yak: web::Json<YakCreate>, pgsql: web::Data<PgPool>) -> 
         }
     }
 }
+/// .
+///
+/// # Panics
+///
+/// Panics if .
 #[instrument]
-pub async fn get_yaks(pgsql: web::Data<PgPool>) -> impl Responder {
+pub async fn get_yaks(pgsql: web::Data<PgPool>, redis: web::Data<redis::Client>) -> impl Responder {
     tracing::info!("Getting yaks");
-    match pgsql_fetch_all_yaks(pgsql).await {
-        Ok(yaks) => HttpResponse::Ok().json(yaks),
-        Err(err) => {
-            tracing::error!("Error: {}", err);
-            HttpResponse::NotFound().body("No yaks found")
+    let yaks = redis_fetch_all_yaks(redis.clone()).await.unwrap();
+    if yaks.len() == 0 {
+        match pgsql_fetch_all_yaks(pgsql).await {
+            Ok(yaks) => {
+                match redis_insert_all_yaks(redis, yaks.clone()).await {
+                    Ok(_) => (),
+                    Err(err) => {
+                        tracing::error!("Error: {}", err);
+                    }
+                }
+                HttpResponse::Ok().json(yaks)
+            }
+            Err(err) => {
+                tracing::error!("Error: {}", err);
+                HttpResponse::NotFound().body("No yaks found")
+            }
         }
+    } else {
+        HttpResponse::Ok().json(yaks)
     }
 }
 
+/// .
 #[instrument]
 pub async fn delete_yak(yak: web::Json<YakDelete>, pgsql: web::Data<PgPool>) -> HttpResponse {
     tracing::info!("Deleting yak: {:?}", yak);
@@ -65,6 +82,7 @@ pub async fn delete_yak(yak: web::Json<YakDelete>, pgsql: web::Data<PgPool>) -> 
     }
 }
 
+/// .
 #[instrument]
 pub async fn update_yak(yak: web::Json<YakUpdate>, pgsql: web::Data<PgPool>) -> HttpResponse {
     tracing::info!("Updating yak: {:?}", yak);
@@ -77,13 +95,33 @@ pub async fn update_yak(yak: web::Json<YakUpdate>, pgsql: web::Data<PgPool>) -> 
     }
 }
 
+/// .
+///
+/// # Panics
+///
+/// Panics if .
 #[instrument]
-pub async fn get_yak(id: web::Path<i32>, pgsql: web::Data<PgPool>) -> HttpResponse {
+pub async fn get_yak(
+    id: web::Path<i32>,
+    pgsql: web::Data<PgPool>,
+    redis: web::Data<redis::Client>,
+) -> HttpResponse {
     tracing::info!("Searching yak: {:?}", id);
-    match pgsql_fetch_yak(pgsql, id.into_inner()).await {
+    let id: i32 = id.into_inner();
+    match redis_fetch_yak(redis.clone(), id).await {
         Ok(yak) => HttpResponse::Ok().json(yak),
         Err(err) => {
             tracing::error!("Error: {}", err);
+            match pgsql_fetch_yak(pgsql, id).await {
+                Ok(yak) => {
+                    redis_insert_yak(redis, yak.clone()).await.unwrap();
+                    HttpResponse::Ok().json(yak);
+                }
+                Err(err) => {
+                    tracing::error!("Error: {}", err);
+                    HttpResponse::NotFound().body("No yak found");
+                }
+            }
             HttpResponse::NotFound().body("No yak found")
         }
     }
